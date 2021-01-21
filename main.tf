023c2dba0a14184f2f485d0614078b926c75ccc2
Lucas Nobre <lnobreteixeira@yahoo.com>
2021-01-20 22:40:58 -0300

    Ajuste de Projeto

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+#### SETUP GOOGLE ####
 terraform {
   required_providers {
     google = {
@@ -6,21 +7,24 @@ terraform {
   }
 }
 
+
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
   version = "3.5.0"
-
 
   project = "oceanic-airway-302321"
   region  = "us-central1"
   zone    = "us-central1-c"
 }
 
+
+#### RECURSOS NO GCP #####
+
 # Cria uma VM no Google Cloud
 resource "google_compute_instance" "firstvm" {
   name         = "website"
   machine_type = "n1-standard-1"
-  zone         = "us-central1-a"
+  zone         = "us-central1-c"
   tags         = ["website", "impacta"]
 
   # Defini a Imagem da VM
@@ -35,10 +39,11 @@ resource "google_compute_instance" "firstvm" {
     network = "default" # Estamos usando a VPC default que já vem por padrão no projeto.
 
     access_config {
-      // A presença do bloco access_config, mesmo sem argumentos, garante que a instância estará acessível pela internet.
     }
+    // A presença do bloco access_config, mesmo sem argumentos, garante que a instância estará acessível pela internet.
   }
 }
+
 # Retorna o IP da VM criada
 output "ip" {
   value = google_compute_instance.firstvm.network_interface.0.access_config.0.nat_ip
