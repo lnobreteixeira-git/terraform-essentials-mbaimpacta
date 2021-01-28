5a26702139d442426de9dd894631cc0875d2ae91
Lucas Nobre <lnobreteixeira@yahoo.com>
2021-01-27 22:20:52 -0300

    Aplicando Boas Práticas no Código

--- main.tf
+++ main.tf
@@ -1,30 +1,10 @@
-#### SETUP GOOGLE ####
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-    }
-  }
-}
-
-
-# Configura o Provider Google Cloud com o Projeto
-provider "google" {
-  version = "3.5.0"
-
-  project = "oceanic-airway-302321"
-  region  = "us-central1"
-  zone    = "us-central1-c"
-}
-
-
 #### RECURSOS NO GCP #####
 
 # Cria uma VM no Google Cloud
 resource "google_compute_instance" "firstvm" {
   name         = "website"
   machine_type = "n1-standard-1"
-  zone         = "us-central1-c"
+  zone         = var.gcp_zone
   tags         = ["website", "impacta"]
 
   # Defini a Imagem da VM
@@ -44,7 +24,3 @@ resource "google_compute_instance" "firstvm" {
   }
 }
 
-# Retorna o IP da VM criada
-output "ip" {
-  value = google_compute_instance.firstvm.network_interface.0.access_config.0.nat_ip
-}
---
+++ variables.auto.tfvars
@@ -0,0 +1,2 @@
+# Variáveis do GCP
+gcp_project   = "oceanic-airway-302321"
