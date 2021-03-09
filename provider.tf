2fcd4f0c19370fb51d79b8a75aa6b92b6087cabd
lnobreteixeira-git <71680589+lnobreteixeira-git@users.noreply.github.com>
2021-03-09 00:25:22 -0300

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,28 @@
+#### SETUP GOOGLE ####
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.58.0"
+    }
+   
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "2.18.0"
+    }
+  }
+}
+
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+  project = var.gcp_project
+  region  = var.gcp_region
+  zone    = var.gcp_zone
+}
+
+# Configura o Provider CloudFlare
+provider "cloudflare" {
+  email   = var.cloudflare_email
+  api_key = var.cloudflare_api_key
+}
