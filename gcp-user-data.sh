b85e40cf19b4012538ac5dbcdb5cd0ebcb6bd324
lnobreteixeira-git <71680589+lnobreteixeira-git@users.noreply.github.com>
2021-03-09 00:12:26 -0300

    Add files via upload

---
+++ gcp-user-data.sh
@@ -0,0 +1,5 @@
+sudo apt-get update 
+sudo apt-get install -y apache2 
+sudo systemctl start apache2 
+sudo systemctl enable apache2 
+echo "<h1> Lab MultiCloud Google Cloud </h1>" | sudo tee /var/www/html/index.html
